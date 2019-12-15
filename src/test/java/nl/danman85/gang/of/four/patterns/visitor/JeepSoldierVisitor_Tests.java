6833da7c8be3df969eb51ad458fffc2a437b08b4
Danman85 <d.versantvoort@hotmail.com>
2019-12-15 12:16:53 +0100

    Upped JUnit tfrom 4 to 5.

--- src/test/java/nl/danman85/gang/of/four/patterns/visitor/JeepSoldierVisitor_Tests.java
+++ src/test/java/nl/danman85/gang/of/four/patterns/visitor/JeepSoldierVisitor_Tests.java
@@ -1,11 +1,11 @@
 package nl.danman85.gang.of.four.patterns.visitor;
 
-import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
-import org.junit.After;
-import org.junit.Before;
-import org.junit.Test;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
 /**
  * JeepSoldierVisitor_Tests
@@ -14,12 +14,12 @@ public class JeepSoldierVisitor_Tests {
 
     private JeepSoldierVisitor jeepSoldierVisitor;
 
-    @Before
+    @BeforeEach
     public void setupTest() {
         this.jeepSoldierVisitor = new JeepSoldierVisitor();
     }
 
-    @After
+    @AfterEach
     public void tearDownTest() {
         this.jeepSoldierVisitor = null;
     }
--- src/test/java/nl/danman85/gang/of/four/patterns/visitor/Sergeant_Tests.java
+++ src/test/java/nl/danman85/gang/of/four/patterns/visitor/Sergeant_Tests.java
@@ -1,10 +1,10 @@
 package nl.danman85.gang.of.four.patterns.visitor;
 
-import static org.junit.Assert.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
-import org.junit.After;
-import org.junit.Before;
-import org.junit.Test;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
 /**
  * Sergeant_Tests
@@ -14,12 +14,12 @@ public class Sergeant_Tests {
     
     private Sergeant sergeant;
 
-    @Before
+    @BeforeEach
     public void setupTest() {
         this.sergeant = new Sergeant();
     }
 
-    @After
+    @AfterEach
     public void tearDownTest() {
         this.sergeant = null;
     }
